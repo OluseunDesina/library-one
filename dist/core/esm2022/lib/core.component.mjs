9a12910e3515cb329008f7dc36da06cc85a34b9d
oluseunDesina <desinaoluseun@gmail.com>
2025-04-10 14:57:07 +0100

    [BUG FIX]: Including the dist folder

---
+++ dist/core/esm2022/lib/core.component.mjs
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import * as i0 from "@angular/core";
+export class CoreComponent {
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "17.3.12", ngImport: i0, type: CoreComponent, deps: [], target: i0.ɵɵFactoryTarget.Component }); }
+    static { this.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "17.3.12", type: CoreComponent, isStandalone: true, selector: "lib-core", ngImport: i0, template: `
+    <p>
+      core works!
+    </p>
+  `, isInline: true, styles: [""] }); }
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "17.3.12", ngImport: i0, type: CoreComponent, decorators: [{
+            type: Component,
+            args: [{ selector: 'lib-core', standalone: true, imports: [], template: `
+    <p>
+      core works!
+    </p>
+  ` }]
+        }] });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29yZS5jb21wb25lbnQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9wcm9qZWN0cy9jb3JlL3NyYy9saWIvY29yZS5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUFFLFNBQVMsRUFBRSxNQUFNLGVBQWUsQ0FBQzs7QUFhMUMsTUFBTSxPQUFPLGFBQWE7K0dBQWIsYUFBYTttR0FBYixhQUFhLG9FQVBkOzs7O0dBSVQ7OzRGQUdVLGFBQWE7a0JBWHpCLFNBQVM7K0JBQ0UsVUFBVSxjQUNSLElBQUksV0FDUCxFQUFFLFlBQ0Q7Ozs7R0FJVCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCB9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5AQ29tcG9uZW50KHtcbiAgc2VsZWN0b3I6ICdsaWItY29yZScsXG4gIHN0YW5kYWxvbmU6IHRydWUsXG4gIGltcG9ydHM6IFtdLFxuICB0ZW1wbGF0ZTogYFxuICAgIDxwPlxuICAgICAgY29yZSB3b3JrcyFcbiAgICA8L3A+XG4gIGAsXG4gIHN0eWxlczogYGBcbn0pXG5leHBvcnQgY2xhc3MgQ29yZUNvbXBvbmVudCB7XG5cbn1cbiJdfQ==
---
+++ dist/core/lib/core.component.d.ts
@@ -0,0 +1,5 @@
+import * as i0 from "@angular/core";
+export declare class CoreComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<CoreComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CoreComponent, "lib-core", never, {}, {}, never, never, true, never>;
+}
